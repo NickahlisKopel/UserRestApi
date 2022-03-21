4a04e2de5718a362e0fe31eb8b5b944e0327d46f
NickahlisKopel <nickahliskopel.student@careerdevs.com>
2022-03-21 18:56:05 -0400

    adds getting user by id and getting users

--- src/main/java/com/careerdevs/userrestapi/controllers/UserController.java
+++ src/main/java/com/careerdevs/userrestapi/controllers/UserController.java
@@ -1,4 +1,38 @@
 package com.careerdevs.userrestapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    Environment env;
+
+    //URL / endpoint http://localhost:4444/api/user/token
+    @GetMapping ("/token")
+    public String getToken (){
+        return env.getProperty("GOREST_TOKEN");
+    }
+
+    //URL / endpoint http://localhost:4444/api/user/{user}
+    @GetMapping("/{user}")
+    public Object userHandler (RestTemplate restTemplate, @PathVariable("user") String user){
+       String apiKey = env.getProperty("GOREST_TOKEN");
+       Object requestData = restTemplate.getForObject("https://gorest.co.in/public/v2/users/" + user, Object.class);
+       return requestData;
+    }
+
+    @GetMapping("/users")
+    public Object rootHandler (RestTemplate restTemplate) {
+        Object requestData = restTemplate.getForObject("https://gorest.co.in/public/v2/users", Object.class);
+        return requestData;
+    }
+
 }
